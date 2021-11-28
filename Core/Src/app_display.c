cba0d1fc8e146fb0b4e49e6ffc118be2c9ce03de
Caroline Alves <sma.caroline@ymail.com>
2021-11-28 12:51:40 -0300

    Mudança na declaração das variáveis

--- Core/Src/app_display.c
+++ Core/Src/app_display.c
@@ -21,25 +21,32 @@ TaskHandle_t hdisplayTask;
 //TaskHandle_t hi2cTask;
 
 /* Declaração dos valores a serem mostrados no display.
- * Velocidade dos eixos dos 3 motores, dados do GPS, velocidade escalar do robô.
+ * Velocidade dos eixos dos 3 motores, dados do GPS, velocidade da base do robô.
  */
 typedef  struct
 {
-	uint16_t v1;
-	uint16_t v2;
-	uint16_t v3;
+	uint32_t w1;
+	uint32_t w2;
+	uint32_t w3;
 } Velocidade_Eixo;
 
 typedef  struct
 {
 	int32_t x;
 	int32_t y;
-	int32_t z;
+	int32_t angulo_teta;
 } GPS;
 
-uint16_t velocidade_escalar;
+typedef  struct
+{
+	uint32_t vx;
+	uint32_t vy;
+	uint32_t w;
+} Velocidade_Base;
+
 Velocidade_Eixo v_eixo;
 GPS gps;
+Velocidade_Base v_base;
 
 /* Variável auxiliar para a conversão dos dados em string */
 char str[32];
@@ -48,16 +55,17 @@ char str[32];
 void start_rtos(void)
 {
 	/* Valores para Teste */
-	v_eixo.v1 = 1;
-	v_eixo.v2 = 2;
-	v_eixo.v3 = 3;
+	v_eixo.w1 = 1;
+	v_eixo.w2 = 2;
+	v_eixo.w3 = 3;
 	gps.x = -20.004;
 	gps.y = 21.544;
-	gps.z = 22.000;
-	velocidade_escalar = 10;
+	gps.angulo_teta = 22.000;
+	v_base.vx = 10;
+	v_base.vy = 10;
+	v_base.w = 10;
 
-	/* Inicializa o display ???? Deve ser incializado nessa parte do código? */
-	ssd1306_Init();
+
 
 	/* Cria a tarefa que irá imprimir os valores em tela */
 	xTaskCreate(displayTask, "displayTask", 128,   NULL,  1,  &hdisplayTask);
@@ -71,6 +79,8 @@ void start_rtos(void)
 
 void displayTask(void *arg)
 {
+	/* Inicializa o display */
+	ssd1306_Init();
 
 	while(1)
 	{
@@ -79,21 +89,21 @@ void displayTask(void *arg)
 		ssd1306_SetCursor(8, 4);
 		ssd1306_WriteString("Velocidade dos eixos", Font_6x8, White);
 
-		sprintf(str, "%u", v_eixo.v1);
+		sprintf(str, "%f", (float)v_eixo.w1);
 		ssd1306_SetCursor(20, 14);
-		ssd1306_WriteString("V1 =", Font_6x8, White);
+		ssd1306_WriteString("W1 =", Font_6x8, White);
 		ssd1306_SetCursor(26, 14);
 		ssd1306_WriteString(str, Font_6x8, White);
 
-		sprintf(str, "%u", v_eixo.v2);
+		sprintf(str, "%f", (float)v_eixo.w2);
 		ssd1306_SetCursor(20, 24);
-		ssd1306_WriteString("V2 =", Font_6x8, White);
+		ssd1306_WriteString("W2 =", Font_6x8, White);
 		ssd1306_SetCursor(26, 24);
 		ssd1306_WriteString(str, Font_6x8, White);
 
-		sprintf(str, "%u", v_eixo.v3);
+		sprintf(str, "%f", (float)v_eixo.w3);
 		ssd1306_SetCursor(20, 34);
-		ssd1306_WriteString("V3 =", Font_6x8, White);
+		ssd1306_WriteString("W3 =", Font_6x8, White);
 		ssd1306_SetCursor(26, 34);
 		ssd1306_WriteString(str, Font_6x8, White);
 
@@ -103,7 +113,7 @@ void displayTask(void *arg)
 
 		/* GPS */
 		ssd1306_SetCursor(8, 4);
-		ssd1306_WriteString("Localização", Font_6x8, White);
+		ssd1306_WriteString("GPS", Font_6x8, White);
 
 		sprintf(str, "%f", (float)gps.x);
 		ssd1306_SetCursor(20, 14);
@@ -117,9 +127,9 @@ void displayTask(void *arg)
 		ssd1306_SetCursor(26, 24);
 		ssd1306_WriteString(str, Font_6x8, White);
 
-		sprintf(str, "%f", (float)gps.z);
+		sprintf(str, "%f", (float)gps.angulo_teta);
 		ssd1306_SetCursor(20, 34);
-		ssd1306_WriteString("Z =", Font_6x8, White);
+		ssd1306_WriteString("t =", Font_6x8, White);
 		ssd1306_SetCursor(26, 34);
 		ssd1306_WriteString(str, Font_6x8, White);
 
@@ -129,12 +139,24 @@ void displayTask(void *arg)
 
 		/* Velocidade escalar */
 		ssd1306_SetCursor(8, 4);
-		ssd1306_WriteString("Velocidade do Robô", Font_6x8, White);
+		ssd1306_WriteString("Velocidade da Base", Font_6x8, White);
 
-		sprintf(str, "%u", velocidade_escalar);
+		sprintf(str, "%f", (float)v_base.vx);
 		ssd1306_SetCursor(20, 14);
-		ssd1306_WriteString("v =", Font_6x8, White);
+		ssd1306_WriteString("Vx =", Font_6x8, White);
 		ssd1306_SetCursor(26, 14);
+		ssd1306_WriteString(str, Font_6x8, White);
+
+		sprintf(str, "%f", (float)v_base.vy);
+		ssd1306_SetCursor(20, 24);
+		ssd1306_WriteString("Vy =", Font_6x8, White);
+		ssd1306_SetCursor(26, 24);
+		ssd1306_WriteString(str, Font_6x8, White);
+
+		sprintf(str, "%f", (float)v_base.w);
+		ssd1306_SetCursor(20, 34);
+		ssd1306_WriteString("W =", Font_6x8, White);
+		ssd1306_SetCursor(26, 34);
 		ssd1306_WriteString(str, Font_6x8, White);
 
 		ssd1306_UpdateScreen(); /* Copia as infromações do Buffer para a tela */
